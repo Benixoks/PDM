9d9ca053b28f694b833ceb4c616df34dbea3dd34
Vitor Suzarte <vitorvsuzarte@gmail.com>
2023-05-30 10:14:38 -0300

    componentizando a página de login

--- my_app/lib/pages/login_page.dart
+++ my_app/lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:my_app/widgets/login/login_form.widget.dart';
 import 'package:my_app/pages/registerPage.dart';
 import 'package:my_app/pages/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -28,9 +29,7 @@ Future signIn() async {
 
 class _LoginPageState extends State<LoginPage> {
   @override
-
-
-  void dispose(){
+  void dispose() {
     _emailController.dispose();
     _senhaController.dispose();
     super.dispose();
@@ -53,134 +52,20 @@ class _LoginPageState extends State<LoginPage> {
                   key: _formKey,
                   child: Column(
                     children: [
-                      // Bem Vindo!
-
                       Text(
-                        'Olá, Torcedor!',
+                        'Olá, Torcedor!\nSeja Bem Vindo ao ClubFLA!',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 30,
                         ),
                       ),
-                      Text( 
-                        'Seja Bem Vindo ao ClubFLA!',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
-                      ),
-
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 30.0),
                         child: Text(
                             'Realize seu login na plaforma oficial do sócio torcedor!',
                             style: TextStyle(fontSize: 20)),
                       ),
-
-                      //TextField EMAIL
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 25.0, vertical: 10),
-                        child: TextFormField(
-                          style: const TextStyle(color: Colors.white),
-                          cursorColor: Colors.white,
-                          controller: _emailController,
-                          validator: (_email) {
-                            if (_email == null || _email.isEmpty) {
-                              return '* Este campo deve ser preenchido!';
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: const OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.greenAccent)),
-                            labelStyle: const TextStyle(color: Colors.white),
-                            labelText: 'E-mail',
-                            hintText: 'exemplo123@gmail.com',
-                            hintStyle: const TextStyle(color: Colors.white),
-                            icon: Icon(Icons.mail),
-                            iconColor: Colors.greenAccent,
-                          ),
-                        ),
-                      ),
-
-                      //TextField SENHA
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                        child: TextFormField(
-                          style: const TextStyle(color: Colors.white),
-                          cursorColor: Colors.white,
-                          autofocus: true,
-                          controller: _senhaController,
-                          validator: (_senha) {
-                            if (_senha == null || _senha.isEmpty) {
-                              return '* Este campo deve ser preenchido!';
-                            }
-                            return null;
-                          },
-                          obscureText: !mostraSenha,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: const OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.greenAccent)),
-                            labelStyle: const TextStyle(color: Colors.white),
-                            labelText: "Senha",
-                            icon: Icon(Icons.key),
-                            iconColor: Colors.greenAccent,
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                mostraSenha
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  mostraSenha = !mostraSenha;
-                                });
-                              },
-                            ),
-                          ),
-                          onChanged: (val) {
-                            if (val.contains(RegExp('[A-Z]')) &&
-                                val.contains(RegExp('[0-9]'))) {
-                              _senha = val;
-                            }
-                          },
-                        ),
-                      ),
-
-                      //Botão de LOGIN
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.greenAccent,
-                          ),
-                          onPressed: () {
-                            // if (_formKey.currentState!.validate()) {
-                            //   logar();
-                               Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomePage(),
-                                 ),
-                               );
-                            // }
-                          },
-                          child: Text('Entrar'),
-                        ),
-                      ),
-
+                      LoginForm(),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
---
+++ my_app/lib/widgets/login/login_form.widget.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_app/widgets/login/login_button.widget.dart';
+import 'package:my_app/common/validations.dart' as validations;
+
+import '../../pages/home_page.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool mostraSenha = false;
+  String _email = '';
+  String _senha = '';
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  // String? emailValidation(email) {
+  //   if (email == null || email.isEmpty) {
+  //     return '* Este campo deve ser preenchido!';
+  //   }
+  //   return null;
+  // }
+
+  // String? passwordValidation(password) {
+  //   if (password == null || password.isEmpty) {
+  //     return '* Este campo deve ser preenchido!';
+  //   }
+  //   return null;
+  // }
+
+  void passwordOnChange(val) {
+    if (val.contains(RegExp('[A-Z]')) && val.contains(RegExp('[0-9]'))) {
+      _senha = val;
+    }
+  }
+
+  void onPressedLogin() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //TextFormeField EMAIL
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+          child: SizedBox(
+            width: 300,
+            child: TextFormField(
+              style: const TextStyle(color: Colors.white),
+              cursorColor: Colors.white,
+              controller: _emailController,
+              validator: validations.emailValidation,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent)),
+                labelStyle: const TextStyle(color: Colors.white),
+                labelText: 'E-mail',
+                hintText: 'exemplo123@gmail.com',
+                hintStyle: const TextStyle(color: Colors.white),
+                icon: const Icon(Icons.mail),
+                iconColor: Colors.greenAccent,
+              ),
+            ),
+          ),
+        ),
+
+        //TextFormeField PASSWORD
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: SizedBox(
+              width: 300,
+              child: TextFormField(
+                style: const TextStyle(color: Colors.white),
+                cursorColor: Colors.white,
+                autofocus: true,
+                controller: _passwordController,
+                validator: validations.passwordValidation,
+                obscureText: !mostraSenha,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.greenAccent)),
+                  labelStyle: const TextStyle(color: Colors.white),
+                  labelText: "Senha",
+                  icon: const Icon(Icons.key),
+                  iconColor: Colors.greenAccent,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      mostraSenha ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        mostraSenha = !mostraSenha;
+                      });
+                    },
+                  ),
+                ),
+                onChanged: passwordOnChange,
+              ),
+            )),
+
+        LoginButton(
+          username: _emailController.text,
+          password: _passwordController.text,
+        )
+      ],
+    );
+  }
+}
