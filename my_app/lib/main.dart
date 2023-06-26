3cbcd20beca3c28250cb9a8e4ab7f00ca56faf23
Benixoks <beniciorafael0@gmail.com>
2023-06-26 13:48:45 -0300

    Restaurando versão anterior

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/home.page.dart';
+import 'package:my_app/pages/login.page.dart';
+import 'package:my_app/pages/register.page.dart';
+import 'package:my_app/pages/config_page.dart';
 import 'package:my_app/pages/main.page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:my_app/provider/user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -9,29 +14,17 @@ void main() async {
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserProvider(),
-      child: const MyApp(),
+      child: MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: ThemeData(
-        colorSchemeSeed: const Color.fromARGB(255, 244, 24, 8),
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
-      theme: ThemeData(
-        colorSchemeSeed: const Color.fromARGB(255, 244, 24, 8),
-        brightness: Brightness.light,
-        useMaterial3: true,
-      ),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MainPage(),
+      home: LoginPage(),
     );
   }
 }
--- my_app/lib/pages/home.page.dart
+++ my_app/lib/pages/home.page.dart
@@ -1,76 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/models/item.model.dart';
-import 'package:my_app/pages/login_page.dart';
+import 'package:my_app/pages/login.page.dart';
 import 'package:my_app/widgets/home/item_card.widget.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}): super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  // List<String> values = [
-  //   'R\$219,90',
-  //   'R\$269,90',
-  //   'R\$399,90',
-  //   'R\$269,90',
-  //   'R\$189,90',
-  //   'R\$269,90',
-  // ];
-  // List<String> valuesImg = [
-  //   'lib/assets/images/camisa1.jpg',
-  //   'lib/assets/images/camisa2.jpg',
-  //   'lib/assets/images/camisa3.jpg',
-  //   'lib/assets/images/camisa4.jpg',
-  //   'lib/assets/images/camisa5.webp',
-  //   'lib/assets/images/camisa6.webp'
-  // ];
-
   List<Item> itens = [];
   var isLoading = false;
+  int _selectedIndex = 0;
 
   Future<void> fetchItens() async {
     await Future.delayed(const Duration(seconds: 1));
     itens = List.generate(
       20,
       (index) => Item(
-          id: 1,
-          price: 189.9,
-          description: "Camisa Framengo palha",
-          tag: "Camiseta",
-          url: 'lib/assets/images/camisa1.jpg'),
+        id: 1,
+        price: 189.9,
+        description: "Camisa Flamengo palha",
+        tag: "Camiseta",
+        url: 'lib/assets/images/camisa1.jpg',
+      ),
     );
     setState(() {});
-  }
-
-  final _homeController = ScrollController();
-  final _searchController = TextEditingController();
-  String search = '';
-  int _selectedIndex = 0;
-
-  static const TextStyle optionStyle = TextStyle(
-      fontSize: 30, fontWeight: FontWeight.bold, color: Colors.greenAccent);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Configurações',
-      style: optionStyle,
-    ),
-    Text(
-      'Sair',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 
   @override
@@ -79,23 +36,28 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-  Widget _buildScreen(int index){
-      switch(index){
-        case 0: 
-          return Home(itens: [],);
-        case 1: 
-          return Config();
-        case 2: 
-          return Sair();
-        default:
-          return const Center(child:Text('Rota inválida'));
-      }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildScreen(int index) {
+    switch (index) {
+      case 0:
+        return Home(itens: itens);
+      case 1:
+        return Config();
+      case 2:
+        return Sair();
+      default:
+        return const Center(child: Text('Rota inválida'));
     }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: automaticallyImplyLeading: false,
       backgroundColor: Colors.blueGrey,
       resizeToAvoidBottomInset: false,
       body: _buildScreen(_selectedIndex),
@@ -104,19 +66,21 @@ class _HomePageState extends State<HomePage> {
         onTap: _onItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Configurações'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Configurações'),
           BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'Sair'),
         ],
         selectedItemColor: Colors.greenAccent,
       ),
     );
-    
   }
 }
 
-class Home extends StatelessWidget {
 
+
+class Home extends StatelessWidget {
   final List<Item> itens;
+
   const Home({required this.itens});
 
   @override
@@ -125,66 +89,26 @@ class Home extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(25.0),
         child: GridView.builder(
-          itemCount: 6,
+          itemCount: itens.length,
           itemBuilder: (context, index) {
-            return ListView(
-              children: [
-                ...itens.map((e) => ItemCard(item: e)).toList()
-                // Card(
-                //   elevation: 10,
-                //   child: Center(
-                //       child: Padding(
-                //     padding: const EdgeInsets.all(25.0),
-                //     child: Column(
-                //       children: [
-                //         Center(
-                //             child: Image(
-                //           image: AssetImage(valuesImg[index]),
-                //           width: 150,
-                //           height: 97,
-                //         )),
-                //         Center(
-                //           child: Text(values[index]),
-                //         ),
-                //       ],
-                //     ),
-                //   )),
-                // ),
-                // Card(
-
-                //   elevation: 10,
-                //   child: Row(
-                //       mainAxisAlignment: MainAxisAlignment.center,
-                //       children: [
-                //         RichText(
-                //           text: TextSpan(
-                //             text: 'Seja um membro Sócio Torcedor',
-                //             style: TextStyle(color: Colors.red),
-                //           ),
-                //         ),
-                //         Text(
-                //           ' e ganhe 25% de desconto nas compras em toda a loja!',
-                //         ),
-                //       ],
-                //     ))
-              ],
-            );
+            final item = itens[index];
+            return ItemCard(item: item);
           },
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
           ),
         ),
-      ),);
-    
+      ),
+    );
   }
 }
+
 class Config extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        
         title: Center(child: Text('Configurações')),
       ),
       body: Container(
@@ -203,63 +127,37 @@ class Config extends StatelessWidget {
             SwitchListTile(
               title: Text('Notificações'),
               value: true,
-              onChanged: (value) {
-                // Implementar a lógica para lidar com a mudança no estado do switch
-              },
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Opções',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            ListTile(
-              title: Text('Idioma'),
-              subtitle: Text('Português'),
-              onTap: () {
-                // Implementar a lógica para a seleção de idioma ou outra ideia
-              },
-            ),
-            Divider(),
-            ListTile(
-              title: Text('Sair'),
-              onTap: () {
-                Navigator.pushReplacement(context, Home(itens: [],) as Route<Object?>);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-    
-  }
-}
+              onChanged: (value) {}
+
+                        ),],),),);            // Implementar a lógica para lidar com a mudança no estado do switch
+  }}
+
 class Sair extends StatelessWidget {
+  const Sair();
+
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
-    
+    return AlertDialog(
+              title: const Text('Tem certeza que deseja sair?'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Fechar o diálogo
+                  },
+                  child: const Text('Cancelar'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      (Route<dynamic> route) => false, // Impede o retorno à página anterior
+                    );
+                  },
+                  child: const Text('Sair'),
+                ),
+              ],
+            );
+      // child: const Text('Sair');
   }
 }
-
-// class _listViewBody extends StatelessWidget {
-//   const _listViewBody({
-//     super.key,
-//     required List<Widget> widgetOptions,
-//     required int selectedIndex,
-//   })  : _widgetOptions = widgetOptions,
-//         _selectedIndex = selectedIndex;
-
-//   final List<Widget> _widgetOptions;
-//   final int _selectedIndex;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: _widgetOptions.elementAt(_selectedIndex),
-//     );
-//   }
-// }
