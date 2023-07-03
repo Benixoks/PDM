4568f4f03090180060b4657558d6435f8bc28170
Vitor Suzarte <vitorvsuzarte@gmail.com>
2023-07-03 00:02:26 -0300

    listItems implementado sem conseguir alterar observable

--- my_app/lib/pages/home.page.dart
+++ my_app/lib/pages/home.page.dart
@@ -1,13 +1,9 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:my_app/models/item.model.dart';
-import 'package:my_app/pages/login.page.dart';
 import 'package:my_app/widgets/home/item_card.widget.dart';
 import 'package:provider/provider.dart';
-
-import '../common/hero.dart';
 import '../stores/item.store.dart';
-import '../stores/user.store.dart';
 import '../widgets/common/bottom_bar.widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -45,7 +41,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final userStore = Provider.of<UserStore>(context, listen: false);
     final itemStore = Provider.of<ItemStore>(context, listen: false);
 
     return Scaffold(
@@ -57,25 +52,6 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (context, index) {
             final item = itens[index];
             return ItemCard(item: item);
-            // return GestureDetector(
-            //   onTap: () {
-            //     Navigator.push(
-            //       context,
-            //       HeroDialogRoute(
-            //         builder: (BuildContext context) {
-            //           return Center(
-            //             child: Hero(
-            //               tag:
-            //                   'item.detail.${item.id}', //implementar lógica de id único por produto
-            //               child: ItemCard(item: item),
-            //             ),
-            //           );
-            //         },
-            //       ),
-            //     );
-            //   },
-            //   child: ItemCard(item: item),
-            // );
           },
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -83,82 +59,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-  }
-}
-
-// class Home extends StatefulWidget {
-//   const Home({super.key});
-
-//   @override
-//   State<Home> createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   // Widget _buildScreen(int index) {
-//   List<Item> itens = [];
-
-//   Future<void> fetchItens() async {
-//     await Future.delayed(const Duration(seconds: 1));
-//     itens = List.generate(
-//       20,
-//       (index) => Item(
-//         id: 1,
-//         price: 189.9,
-//         description: "Camiseta Flamengo Oficial",
-//         tag: "Camiseta Flamengo Oficial Preta e Vermelha",
-//         url: 'lib/assets/images/camisa1.jpg',
-//       ),
-//     );
-//     setState(() {});
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final userStore = Provider.of<UserStore>(context, listen: false);
-//     final itemStore = Provider.of<ItemStore>(context, listen: false);
-
-//     return Scaffold(
-//       bottomNavigationBar: const BottomBar(),
-//       body: Padding(
-//         padding: const EdgeInsets.all(10.0),
-//         child: GridView.builder(
-//           itemCount: min(10, itens.length),
-//           itemBuilder: (context, index) {
-//             final item = itens[index];
-//             return GestureDetector(
-//               onTap: () {
-//                 Navigator.push(
-//                   context,
-//                   HeroDialogRoute(
-//                     builder: (BuildContext context) {
-//                       return Center(
-//                         child: Hero(
-//                           tag:
-//                               'item.detail.${item.id}', //implementar lógica de id único por produto
-//                           child: ItemCard(item: item),
-//                         ),
-//                       );
-//                     },
-//                   ),
-//                 );
-//               },
-//               child: ItemCard(item: item),
-//             );
-//           },
-//           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//             crossAxisCount: 2,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-class Sair extends StatelessWidget {
-  const Sair();
-
-  @override
-  Widget build(BuildContext context) {
-    return LoginPage();
   }
 }
